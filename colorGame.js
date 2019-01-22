7c99eac6f02d65bbf451af040dd5c17cfe2a3635
Casperfeng <casp.feng@gmail.com>
2019-01-22 11:47:06 +0100

    added click listeners to elements

--- colorGame.js
+++ colorGame.js
@@ -1,9 +1,28 @@
 let colors = [] = [
   "rgb(255, 0, 0)",
-  "rgb(255, 0, 0)",
-  "rgb(255, 0, 0)",
-  "rgb(255, 0, 0)",
-  "rgb(255, 0, 0)",
-  "rgb(255, 0, 0)"
-
+  "rgb(255, 255, 0)",
+  "rgb(0, 255, 0)",
+  "rgb(0, 255, 255)",
+  "rgb(0, 0, 255)",
+  "rgb(255, 0, 255)"
 ]
+
+//All squares
+let squares = document.querySelectorAll(".square");
+
+for (let i = 0; i < squares.length; i++ ){
+  squares[i].style.backgroundColor = colors[i];
+}
+
+//Add click listeners to squares
+squares[i].addEventListener("click", function(){
+  //Grab color of clicked square
+  let clickedColor = this.style.background;
+  //Compare color to pickedColor
+  if (clickedColor === pickedColor){
+    alert("Correct!");
+  } else {
+    alert("Wrong");
+  }
+}); 
+
